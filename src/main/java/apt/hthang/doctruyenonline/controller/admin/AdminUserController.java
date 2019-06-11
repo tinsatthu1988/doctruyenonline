b30c5a225c917bc2c8bed6c86fac767e591d93e0
Nguyễn Huy Thắng <nhthang304@gmail.com>
2019-06-11 11:36:01 +0700

    update

--- src/main/java/apt/hthang/doctruyenonline/controller/admin/AdminUserController.java
+++ src/main/java/apt/hthang/doctruyenonline/controller/admin/AdminUserController.java
@@ -4,23 +4,24 @@ import apt.hthang.doctruyenonline.component.MyComponent;
 import apt.hthang.doctruyenonline.entity.MyUserDetails;
 import apt.hthang.doctruyenonline.entity.User;
 import apt.hthang.doctruyenonline.exception.NotFoundException;
-import apt.hthang.doctruyenonline.service.ChapterService;
 import apt.hthang.doctruyenonline.service.RoleService;
 import apt.hthang.doctruyenonline.service.UserService;
 import apt.hthang.doctruyenonline.utils.ConstantsListUtils;
 import apt.hthang.doctruyenonline.utils.ConstantsStatusUtils;
 import apt.hthang.doctruyenonline.utils.ConstantsUtils;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 /**
@@ -38,7 +39,7 @@ public class AdminUserController {
     private UserService userService;
     @Autowired
     private MyComponent myComponent;
-        
+    
     //Lấy Thông Tin Tên User Login, Avatar User Login
     private void getUser(Model model, Principal principal) throws NotFoundException {
         MyUserDetails loginedUser = (MyUserDetails) ((Authentication) principal).getPrincipal();
@@ -78,7 +79,7 @@ public class AdminUserController {
             throw new NotFoundException("Tài khoản của bạn đã bị khóa mời liên hệ admin để biết thêm thông tin");
         }
         User editUser = userService.findUserById(id);
-
+        
         if (editUser == null) {
             redirectAttrs.addFlashAttribute("checkEditUserFalse", "Người dùng không tồn tại");
             return "redirect:/quan-tri/nguoi_dung";
@@ -87,11 +88,49 @@ public class AdminUserController {
         if (!model.containsAttribute("user")) {
             model.addAttribute("user", editUser);
         }
-
-        model.addAttribute("listRole" , roleService.getAllRole());
-
+        
+        model.addAttribute("listRole", roleService.getAllRole());
+        
         model.addAttribute("statusList", ConstantsListUtils.LIST_CATEGORY_STATUS_VIEW_ALL);
         
         return "dashboard/editUserPage";
+    }
+    
+    @PostMapping("/cap_nhat/save")
+    public String saveUpdateUser(@Valid User user, BindingResult result, Principal principal,
+                                 RedirectAttributes redirectAttributes) throws NotFoundException {
+        MyUserDetails loginedUser = (MyUserDetails) ((Authentication) principal).getPrincipal();
+        User userLogin = userService.findUserById(loginedUser.getUser().getId());
+        if (userLogin == null) {
+            throw new NotFoundException("Tài khoản không tồn tại mời liên hệ admin để biết thêm thông tin");
+        }
+        if (userLogin.getStatus().equals(ConstantsStatusUtils.USER_DENIED)) {
+            throw new NotFoundException("Tài khoản của bạn đã bị khóa mời liên hệ admin để biết thêm thông tin");
+        }
+        
+        User editUser = userService.findUserById(user.getId());
+        
+        if (editUser == null) {
+            redirectAttributes.addFlashAttribute("checkEditUserFalse", "Người dùng cập nhật không tồn tại");
+            return "redirect:/quan-tri/nguoi_dung";
+        }
+        
+        boolean hasError = result.hasErrors();
+        if (hasError) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", result);
+            redirectAttributes.addFlashAttribute("user", user);
+            return "redirect:/quan-tri/nguoi_dung/cap_nhat/" + user.getId();
+        }
+        editUser.setStatus(user.getStatus());
+        editUser.setRoleList(user.getRoleList());
+        try {
+            userService.updateUser(editUser);
+            redirectAttributes.addFlashAttribute("checkEditUserTrue", "Cập Nhật Thành Công");
+            return "redirect:/quan-tri/nguoi_dung";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("checkEditUserFalse", "Có lỗi xảy ra!");
+            return "redirect:/quan-tri/nguoi_dung";
+        }
+        
     }
 }
